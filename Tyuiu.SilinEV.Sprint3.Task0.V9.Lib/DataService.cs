84d8068ddfc606d2c62171f7975982ffc011ff58
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-09 17:31:45 +0500

    Add project files.

---
+++ Tyuiu.SilinEV.Sprint3.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SilinEV.Sprint3.Task0.V9.Lib
+{
+    public class DataService : ISprint3Task0V9
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double a = 0;
+            
+            for (Convert.ToInt32(startValue); startValue <= stopValue; startValue++)
+            {
+                a += Math.Pow(1 / (3 + Math.Pow(value, startValue)), startValue);
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint3.Task0.V9/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.SilinEV.Sprint3.Task0.V9.Lib;
+
+DataService ds = new DataService();
+Console.WriteLine(ds.GetSumSeries(0.5, 1, 15));
