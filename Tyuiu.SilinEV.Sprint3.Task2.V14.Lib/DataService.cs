f34c3897eeda4e6caf291d3903a830efbcb5963e
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-26 10:53:56 +0500

    Task3

---
+++ Tyuiu.SilinEV.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SilinEV.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double a = 1;
+            do
+            {
+                a *= Math.Pow(value / startValue, 3);
+                value += 1;
+            }
+            while (a < stopValue);
+
+            return Math.Round(a, 3);
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint3.Task2.V14/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.SilinEV.Sprint3.Task2.V14.Lib;
+
+DataService ds = new();
+Console.WriteLine(ds.GetMultiplySeries(5, 1, 6));
