80501e119f044d598ca0581e2bd1a30088b8f9fe
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 20:59:20 +0500

    Task4

---
+++ Tyuiu.SilinEV.Sprint3.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SilinEV.Sprint3.Task4.V1.Lib
+{
+    public class DataService : ISprint3Task4V1
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double a = 0;
+
+            for (int i = startValue; a <= stopValue; startValue++)
+            {
+                if (startValue == 0)
+                {
+                    break;
+                }
+                a += Math.Sin(startValue) / startValue;
+            }
+
+            return Math.Pow(a, 3);
+        }
+    }
+}
