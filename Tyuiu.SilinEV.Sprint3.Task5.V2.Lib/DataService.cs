c469e3012340cc168986292f424fc8011a3c9aae
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 21:16:40 +0500

    Task5

---
+++ Tyuiu.SilinEV.Sprint3.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SilinEV.Sprint3.Task5.V2.Lib
+{
+    public class DataService : ISprint3Task5V2
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double a = 0;
+
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int j = startValue2; j <= stopValue2; j++)
+                {
+                    a += (Math.Cos(j) + x) / x;
+                }
+            }
+
+            return Math.Round(a);
+        }
+    }
+}
