24ec304fb73beff0d8e844d1e433e8cdc52d06cf
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 20:49:22 +0500

    task3

---
+++ Tyuiu.SilinEV.Sprint3.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint3.Task3.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
