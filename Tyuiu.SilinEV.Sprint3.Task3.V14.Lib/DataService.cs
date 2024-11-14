4f9e73ce3bff71da7394eef99c9b3a73012bfced
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-14 19:55:41 +0500

    Task3

---
+++ Tyuiu.SilinEV.Sprint3.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SilinEV.Sprint3.Task3.V14.Lib
+{
+    public class DataService : ISprint3Task3V14
+    {
+        public string ReplaceCharInString(string value, char replaceable, char replacement)
+        {
+            string a = "";
+            foreach (char c in value)
+            {
+                if (c == replaceable)
+                {
+                    a += "’";
+                }
+                else
+                {
+                    a += c;
+                }
+            }
+            return a;
+        }
+    }
+}
